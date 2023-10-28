bd9b21715c8f2550e616f04ef17f7593edb6a497
Dillon Vaughan <djv8@students.uwf.edu>
2023-10-28 15:03:54 -0500

    Code clean up

--- src/main/java/com/coderiders/userservice/utilities/UserServiceQueries.java
+++ src/main/java/com/coderiders/userservice/utilities/UserServiceQueries.java
@@ -2,8 +2,6 @@ package com.coderiders.userservice.utilities;
 
 public class UserServiceQueries {
 
-
-
     public static String getAllUsersThatAreNotBlocked(String requestingClerkId) {
         StringBuffer sqlQuery = new StringBuffer();
         sqlQuery.append("SELECT u.username,\n" +
@@ -21,18 +19,4 @@ public class UserServiceQueries {
                 "AND clerk_id != '" + requestingClerkId + "';");
         return String.format(sqlQuery.toString());
     }
-
-    String query = "SELECT u.username,\n" +
-            "       u.first_name,\n" +
-            "       u.last_name,\n" +
-            "       u.image_url,\n" +
-            "       u.clerk_id\n" +
-            "FROM Users u\n" +
-            "WHERE NOT EXISTS (\n" +
-            "    SELECT 1 FROM Friends\n" +
-            "    WHERE (user_clerk_id1 = u.clerk_id OR user_clerk_id2 = u.clerk_id)\n" +
-            "    AND (user_clerk_id1 = 'user_2WAMNVez4us6P87bQk6806lokQe' OR user_clerk_id2 = 'user_2WAMNVez4us6P87bQk6806lokQe')\n" +
-            "    AND (status = 'CONFIRMED' OR status = 'BLOCKED')\n" +
-            ")\n" +
-            "AND clerk_id != 'user_2WAMNVez4us6P87bQk6806lokQe';";
 }
